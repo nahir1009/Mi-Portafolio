fe7a5e6f401a0ba53ac47a424bde4245e82b6a6e
nahir1009 <nahir1009@gmail.com>
2025-06-02 23:39:48 -0300

    guardo cambio modales

---
+++ modalcertificaciones.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const certificadosModal = document.querySelector('.modal-certificados-contenido');
+
+    // nuevo modal de imagen grande
+    const imagenGrandeModalCheckbox = document.getElementById('modal-imagen-grande');
+    const imagenCertificadoGrande = document.getElementById('imagen-certificado-grande');
+    const modalImagenGrandeTitulo = document.getElementById('modal-imagen-grande-titulo');
+    const ventanaImagenGrande = document.getElementById('ventana-imagen-grande');
+
+
+    if (certificadosModal) {
+        certificadosModal.addEventListener('click', (event) => {
+            if (event.target.tagName === 'IMG') {
+                const clickedImageSrc = event.target.src;
+                const clickedImageAlt = event.target.alt;
+
+               
+                imagenCertificadoGrande.src = clickedImageSrc;
+                imagenCertificadoGrande.alt = clickedImageAlt;
+                modalImagenGrandeTitulo.textContent = clickedImageAlt || "Certificado"; 
+
+                
+                imagenGrandeModalCheckbox.checked = true;
+
+                
+            }
+        });
+    }
+
+  
+    const certificadoImages = document.querySelectorAll('.modal-certificados-contenido img');
+    certificadoImages.forEach(img => {
+        img.style.cursor = 'pointer'; 
+    });
+});
+
+// Función para actualizar el reloj en la barra de tareas
+function actualizarReloj() {
+    const relojElement = document.querySelector('.reloj p');
+    if (relojElement) {
+        const ahora = new Date();
+        let horas = ahora.getHours();
+        let minutos = ahora.getMinutes();
+        const ampm = horas >= 12 ? 'PM' : 'AM';
+
+        horas = horas % 12;
+        horas = horas ? horas : 12; 
+        minutos = minutos < 10 ? '0' + minutos : minutos;
+
+        relojElement.textContent = `${horas}:${minutos} ${ampm}`;
+    }
+}
+
+// Llamo a la función para actualizar el reloj cada segundo
+setInterval(actualizarReloj, 1000);
+
+// Llamo  al cargar para mostrar la hora inicial
+actualizarReloj();
